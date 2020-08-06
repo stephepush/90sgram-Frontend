7cdfca2025d07a65e1ae1812194dbe4919cc9bf1
stephepush <7speter@gmail.com>
2020-08-06 17:19:02 -0400

    Adds searchbar html/jsx

--- src/Logo.js
+++ src/Logo.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function Logo() {
     return(
-        <img src="" alt="90sgram Logo"></img>
+        //<img src="" alt="90sgram Logo"></img>
+        <h1>90sGram</h1>
     )
 }
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function SearchBar() {
     return(
         <div>
-
+            <input type="search" id="site-search" 
+                name="query" 
+                aria-label="Search 90sgram" placeholder="Search 90sgram" required>
+                
+            </input>
         </div>
     )
 }
