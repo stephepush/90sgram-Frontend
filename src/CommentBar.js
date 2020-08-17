db0354c7dab1aaeb9346e5fc8c0184858f49a23b
stephepush <7speter@gmail.com>
2020-08-17 08:04:10 -0400

    Adds data in data.js

--- src/CommentBar.js
+++ src/CommentBar.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function CommentBar() {
     return(
-        <input>
+        <section class="comment-bar">
+            <input class="comment-input" placeholder="Add a comment... "/>
+            <input type="submit" value="Post" class="comment-submit"/>
+        </section>
         
-        </input>
+        
     )
 }
 
--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -3,9 +3,17 @@ import Likes from "./Likes";
 import CommentBar from "./CommentBar";
 import CommentContainer from "./CommentContainer";
 import './index.css';
+import styled from 'styled-components';
+
+const Section = styled.section`
+display: grid;
+
+grid-template-rows: 1fr 4fr 1fr;
+height: 600px;
+`
 
 
-export default function ImageContainer() {
+export default function ImageContainer(props) {
 
     return(
     <main className="image-container">
